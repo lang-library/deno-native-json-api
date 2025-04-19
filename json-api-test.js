d811c7b0d3208b43bdb5599751430e52f51118e1
JavaCommons Technologies <javacommons@gmail.com>
2025-04-19 15:38:48 +0900

    .

--- json-api-test.js
+++ json-api-test.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env -S deno run --allow-all --unstable-ffi
-//import { NativeJsonApi } from "https://github.com/lang-library/deno-lib01/raw/2024.0203.1540/json-dll.js";
-import { NativeJsonApi } from "./NativeJsonApi.js";
+import { NativeJsonApi } from "https://github.com/lang-library/deno-native-json-api/raw/2025.0419.1536.24/NativeJsonApi.js";
+//import { NativeJsonApi } from "./NativeJsonApi.js";
 
 const libName = "PROGRAM.dll"
 var dll = new NativeJsonApi(libName);
