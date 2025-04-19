21e8f8e0055947c80ef732f4272cfe09e18940f2
JavaCommons Technologies <javacommons@gmail.com>
2025-04-19 15:27:20 +0900

    2025.0419.1527.20

---
+++ release.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+set -uvx
+set -e
+cd "$(dirname "$0")"
+cwd=`pwd`
+ts=`date "+%Y.%m%d.%H%M.%S"`
+version="${ts}"
+
+tag="$version"
+cd $cwd
+git add .
+git commit -m"$tag"
+git tag -a "$tag" -m"$tag"
+git push origin "$tag"
+git push origin HEAD:main
+git remote -v
