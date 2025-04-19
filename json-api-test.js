f3ae5988deed0836561b9c67d2d7ae1863377bb5
JavaCommons Technologies <javacommons@gmail.com>
2025-04-19 15:39:43 +0900

    .

--- json-api-test.js
+++ json-api-test.js
@@ -2,7 +2,7 @@
 import { NativeJsonApi } from "https://github.com/lang-library/deno-native-json-api/raw/2025.0419.1536.24/NativeJsonApi.js";
 //import { NativeJsonApi } from "./NativeJsonApi.js";
 
-const libName = "PROGRAM.dll"
-var dll = new NativeJsonApi(libName);
-console.log(dll.Call("add2", [33, 44]));
-console.log(dll.Call("add2", [33, 44, 55]));
+const $libName = "PROGRAM.dll"
+var $api = new NativeJsonApi($libName);
+console.log($api.Call("add2", [33, 44]));
+console.log($api.Call("add2", [33, 44, 55]));
